20f6529a1073453aa79913f6fa42605e6084a4ec
Nikita Provkov <sp_8@list.ru>
2024-09-02 16:05:12 +0300

    Add pages repository mongodb realization

---
+++ microservices/urls_service/app/repositories/pages/pages_mongodb.py
@@ -0,0 +1,37 @@
+from fastapi import HTTPException
+from motor.motor_asyncio import AsyncIOMotorCollection
+from pydantic import AnyHttpUrl
+
+from app.repositories.pages.pages_protocol import PagesRepositoryProtocol
+from app.schemas.pages_schemas import PageInfoGet
+
+
+class PagesRepositoryMongodb(PagesRepositoryProtocol):
+
+    def __init__(self, collection: AsyncIOMotorCollection):
+        self.collection = collection
+
+    async def get_pages(self, url: str | None = None,
+                        title: str | None = None) -> list[PageInfoGet]:
+        try:
+            query = {}
+            if url:
+                query["url"] = {"$regex": f".*{url}.*", "$options": "i"}
+            if title:
+                query["title"] = {"$regex": f".*{title}.*", "$options": "i"}
+            cursor = self.collection.find(query)
+
+            pages = []
+            async for page in cursor:
+                pages.append(PageInfoGet(url=page["url"], title=page["title"]))
+
+            return pages
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error retrieving pages: {e}")
+
+    async def get_page_html(self, url: AnyHttpUrl) -> str | None:
+        try:
+            document = await self.collection.find_one({"url": str(url)})
+            return document.get("html", None)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error retrieving page HTML content: {e}")
