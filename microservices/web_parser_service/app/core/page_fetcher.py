eaa142c302a8ef9f99747fd1d475f01eee5b89ca
Nikita Provkov <sp_8@list.ru>
2024-09-02 16:13:36 +0300

    Add class for page fetching

---
+++ microservices/web_parser_service/app/core/page_fetcher.py
@@ -0,0 +1,32 @@
+from aiohttp import ClientSession, ClientResponse
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+from app.core.config import settings
+from app.core.logger import logger
+
+
+class PageFetcher:
+    headers: dict[str, str] | None = None
+
+    def __init__(self, headers: dict[str, str] | None = None):
+        self.headers = headers
+
+    @retry(
+        stop=stop_after_attempt(settings.retries_count),
+        wait=wait_exponential(multiplier=1, min=1, max=10)
+    )
+    async def get_html(self, session: ClientSession, url: str) -> str | None:
+        async with session.get(url, headers=self.headers, timeout=settings.request_timeout_in_seconds) as response:
+            if self.check_html(response, url):
+                return await response.text()
+            logger.warn(f"Can't fetch html from {url}")
+            return None
+
+    @staticmethod
+    def check_html(response: ClientResponse, url: str) -> bool:
+        if not 200 <= response.status < 300:
+            logger.warn(f"Incorrect status code: {response.status} from {url}")
+            return False
+
+        content_type = response.headers.get("Content-Type", "").lower()
+        return "html" in content_type
