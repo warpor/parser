4db159c44c272dae1af52e7418d4e2043b195717
Nikita Provkov <sp_8@list.ru>
2024-09-02 16:03:42 +0300

    Add config for urls_service

---
+++ microservices/urls_service/app/core/config.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    DB = os.getenv("DB")
+    DB_HOST: str | None = os.getenv("DB_HOST")
+    DB_PORT: str | None = os.getenv("DB_PORT")
+    DB_NAME: str | None = os.getenv("DB_NAME")
+    DB_COLLECTION: str | None = os.getenv("DB_COLLECTION")
+
+    BROKER_PROTOCOL: str | None = os.getenv("BROKER_PROTOCOL")
+    BROKER_HOST: str | None = os.getenv("BROKER_HOST")
+    BROKER_PORT: str | None = os.getenv("BROKER_PORT")
+    BROKER_USER: str | None = os.getenv("BROKER_DEFAULT_USER")
+    BROKER_PASS: str | None = os.getenv("BROKER_DEFAULT_PASS")
+    BROKER_START_URLS_QUEUE: str | None = os.getenv("BROKER_START_URLS_QUEUE")
+
+    def get_database_url(self) -> str:
+        xml = f"{self.DB}://{self.DB_HOST}:{self.DB_PORT}"
+        print(xml)
+        return xml
+
+    def get_broker_url(self) -> str:
+        return (f"{self.BROKER_PROTOCOL}://{self.BROKER_USER}:"
+                f"{self.BROKER_PASS}@{self.BROKER_HOST}:{self.BROKER_PORT}")
+
+
+settings = Settings()
