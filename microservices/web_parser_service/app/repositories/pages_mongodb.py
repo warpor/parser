68697818b9c4674abe31ed0d0e343e2b3d878035
Nikita Provkov <sp_8@list.ru>
2024-09-03 11:14:42 +0500

    Add logging for bulk insert
    Log info, if url duplicate

--- microservices/web_parser_service/app/repositories/pages_mongodb.py
+++ microservices/web_parser_service/app/repositories/pages_mongodb.py
@@ -1,7 +1,8 @@
-from motor.motor_asyncio import AsyncIOMotorCollection
-
+from app.core.logger import logger
 from app.repositories.pages_protocol import PagesRepositoryProtocol
 from app.schemas.pages_schemas import PageToDb
+from motor.motor_asyncio import AsyncIOMotorCollection
+from pymongo.errors import BulkWriteError
 
 
 class PagesRepositoryMongodb(PagesRepositoryProtocol):
@@ -13,5 +14,10 @@ class PagesRepositoryMongodb(PagesRepositoryProtocol):
         try:
             await self.collection.insert_many(
                 (page.dict() for page in pages), ordered=False)
-        except Exception:
-            pass
+        except BulkWriteError as e:
+            write_errors: list = e.details.get("writeErrors", [])
+            for write_error in write_errors:
+                error: str = write_error.get("errmsg")
+                logger.warning(f"Bulk write warn: {error}")
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
